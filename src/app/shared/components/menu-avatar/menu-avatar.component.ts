f34a36be9ce1a79d615275eeb9b706cc21553d01
Fabio Victor <fabinhovictor43@hotmail.com>
2024-08-12 00:39:04 -0300

    iniciando componentes da estrutura base

---
+++ src/app/shared/components/menu-avatar/menu-avatar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-avatar',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-avatar.component.html',
+  styleUrl: './menu-avatar.component.scss'
+})
+export class MenuAvatarComponent {
+
+}
